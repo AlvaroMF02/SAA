409930e8c40424b36a13c147e3ffbae72b27bc5e
albaro <alvaromarflo@gmail.com>
2025-04-07 17:42:28 +0200

    practica evaluable

--- start_python_env.sh
+++ start_python_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VENV_DIR="/home/albaro/python_envs/venv1"
+VENV_DIR="/home/albaro/Documentos/instalacion/python_envs/venv1"
 source $VENV_DIR/bin/activate
 jupyter lab --notebook-dir=$(pwd)
 
